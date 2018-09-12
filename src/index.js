ea4d7f04f593b8f462bc59218e31366051c9faa4
Ivan <ivanmhale@gmail.com>
2018-09-12 11:16:13 -0400

    restructure Header

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-import Header from './components/header';
-import SearchBar from './components/search_bar';
-import Videolist from './components/video_list';
-import VideoDetail from './components/video_details';
-import registerServiceWorker from './registerServiceWorker';
-import _ from 'lodash';
+import SearchBar from "./components/search_bar";
+import Videolist from "./components/video_list";
+import VideoDetail from "./components/video_details";
+import registerServiceWorker from "./registerServiceWorker";
+import _ from "lodash";
 import "./index.css";
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-const API_KEY = 'AIzaSyDrJ9e6MXeuoBK0JkXhj5eDDr34vy6mOmQ';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import API_KEY from "./key.js";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
 
-    this.state={
-      videos:[],
-      selectedVideo: null,
-      };
-
-    this.videoSearch('star wars: tie fighter');
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
   }
 
-  videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos)=>{
+  componentDidMount() {
+    this.videoSearch("star wars: tie fighter");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
-        selectedVideo: videos[0],
+        selectedVideo: videos[0]
       });
     });
   }
 
-  render(){
-    const videoSearch=_.debounce((term)=>{this.videoSearch(term)}, 300);
-  return (
-        <div id="app">
-          <Header/>
-          <SearchBar onSearchTermChange={videoSearch}/>
-          <VideoDetail video={this.state.selectedVideo}/>
-          <Videolist
-          onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
-          videos={this.state.videos}/>
-        </div>
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+    return (
+      <div id="app">
+        <section className='header'>
+          <h1>YouTube Viewer</h1>
+        </section>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <Videolist
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
---
+++ src/key.js
@@ -0,0 +1,2 @@
+const API_KEY = "AIzaSyBx-SiqS3Qv5sBQ59CHOQ7HTl_dYwjDYRU";
+export default API_KEY;
